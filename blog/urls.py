68bd530105c636b8ec5e4d7beced9c58c7441504
nilam23 <nilampathak72@gmail.com>
2024-03-30 00:09:58 +0530

    add: get_specific_blog api

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
   path('blog', views.create_blog, name='create_blog'),
   path('blogs', views.get_all_blogs, name='get_all_blogs'),
+  path('blogs/<str:blog_id>', views.get_specific_blog, name='get_specific_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -47,4 +47,19 @@ def get_all_blogs(request):
 
     return JsonResponse({ 'data': blogs }, status=status.HTTP_200_OK)
   except Exception as error:
-    return helpers.handle_view_exception(error, 'Exception in create_blog view')
+    return helpers.handle_view_exception(error, 'Exception in get_all_blogs view')
+  
+@api_view(['GET'])
+def get_specific_blog(request, blog_id):
+  try:
+    db_instance = request.db
+    aggregation_query = queries.aggregation_query_for_blogs({ '_id': ObjectId(blog_id) })
+
+    blogs = blog_services.find_all_blogs(db_instance, aggregation_query)
+
+    if not len(blogs):
+      return JsonResponse({ 'error_message': f'Blog with id {blog_id} not found' }, status=status.HTTP_404_NOT_FOUND)
+    
+    return JsonResponse({ 'data': blogs[0] }, status=status.HTTP_200_OK)
+  except Exception as error:
+    return helpers.handle_view_exception(error, 'Exception in get_specific_blog view')
